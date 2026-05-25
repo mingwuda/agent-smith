819469cb24ebc70a9bb3691e58c1f840a4232273
Mingo <mingwuda@163.com>
2026-05-25 13:38:17 +0800

    优化脚本

--- packaging/windows/build.cmd
+++ packaging/windows/build.cmd
@@ -34,8 +34,12 @@ if not exist "%PYTHON%" (
 "%PYTHON%" -m pip install --upgrade pip
 if errorlevel 1 exit /b 1
 
-"%PYTHON%" -c "import platform,sys; print('cp%d%d-%s' % (sys.version_info[0], sys.version_info[1], 'win_amd64' if platform.machine().lower() in ('amd64','x86_64') else 'win32'))" > "%WHEEL_TAG_FILE%"
+"%PYTHON%" -c "import platform,sys; arch='win_amd64' if platform.machine().lower() in ('amd64','x86_64') else 'win32'; print(f'cp{sys.version_info[0]}{sys.version_info[1]}-{arch}')" > "%WHEEL_TAG_FILE%"
 set /p WHEEL_TAG=<"%WHEEL_TAG_FILE%"
+if "%WHEEL_TAG%"=="" (
+  echo Error: failed to detect Python wheel tag.
+  exit /b 1
+)
 set "WHEEL_DIR=%ROOT%\dep\windows\%WHEEL_TAG%"
 
 if exist "%WHEEL_DIR%" (
